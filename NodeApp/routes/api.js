bc72838ab1c80a1bcc1ca735fe6628e61442e33b
PedroG9 <pedrogarciatalavera1997@hotmail.com>
2020-06-17 09:39:58 +0200

    Creación api/usuarios

--- NodeApp/routes/api.js
+++ NodeApp/routes/api.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const apiAlojamientosRouter = require('./api/alojamientos');
+const apiUsuariosRouter = require('./api/usuarios');
 
 router.use('/alojamientos', apiAlojamientosRouter);
+router.use('/usuarios', apiUsuariosRouter);
 
 module.exports = router;
